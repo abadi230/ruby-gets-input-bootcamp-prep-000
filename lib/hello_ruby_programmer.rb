c15b371deac0c16aca3fde7348b9cb3becc17e52
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-30 13:23:12 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -2,8 +2,8 @@ def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 
-inputName= gets.to_s 
-greeting(inputName)
+# inputName= gets.to_s 
+# greeting(inputName)
 # return in new line  
 # => 'Hi Abdullah,
 # ! Welcome to the wonderful world of ruby programming.'
